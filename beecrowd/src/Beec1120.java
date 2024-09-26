5febb24406069929c2ad1b729a562ff5b39c4ed0
umkolove <umarbedelbaev@gmail.com>
2024-09-26 23:10:11 +0600

    complete 1120

--- beecrowd/src/Beec1120.java
+++ beecrowd/src/Beec1120.java
@@ -4,19 +4,18 @@ public class Beec1120 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("цифру введи: ");
+        System.out.print("Введи цифру: ");
         String brokenDigit = scanner.next();
 
-        System.out.print("введи значение: ");
+        System.out.print("Введи значение: ");
         String contractValue = scanner.next();
 
         String fixedValue = contractValue.replace(brokenDigit, "");
 
         fixedValue = fixedValue.replaceFirst("^0+", "");
 
-        System.out.println("ответ: " + fixedValue);
+        System.out.println("Ответ: " + fixedValue);
 
         System.out.println(fixedValue.isEmpty() ? "0" : fixedValue);
     }
 }
-
